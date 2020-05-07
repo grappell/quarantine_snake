e287879ae8a30e18e6a67e13cc6fb0cbf7c502c2
Grayson <Grayson@HCLPT109.hellerint.local>
2020-05-06 17:20:38 -0700

    Updates

---
+++ precache-manifest.c23f05b7ddc3ca98a0f3926fc1dd7ac1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c2a8a8efb6301bd1624a0de5f08037b",
+    "url": "/quarantine_snake/index.html"
+  },
+  {
+    "revision": "503e485cb8674eb69080",
+    "url": "/quarantine_snake/static/css/main.82cbb91d.chunk.css"
+  },
+  {
+    "revision": "9ddc74eabecbea2de09b",
+    "url": "/quarantine_snake/static/js/2.eefad24d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/quarantine_snake/static/js/2.eefad24d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "503e485cb8674eb69080",
+    "url": "/quarantine_snake/static/js/main.1857b30b.chunk.js"
+  },
+  {
+    "revision": "6c30c91e5252fcf4c856",
+    "url": "/quarantine_snake/static/js/runtime-main.ac20cafc.js"
+  }
+]);
